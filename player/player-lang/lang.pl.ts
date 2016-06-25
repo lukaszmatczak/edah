7c6fb047536704aeab6529e217f272a91b4ee746
Lukasz Matczak <lukasz1235@gmail.com>
2016-06-25 12:02:58 +0200

    Player - polish translation

---
+++ player/player-lang/lang.pl.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>BigPanel</name>
+    <message>
+        <location filename="../bigpanel.cpp" line="193"/>
+        <source>Autoplay</source>
+        <translation>Autoodtwarzanie</translation>
+    </message>
+</context>
+<context>
+    <name>Player</name>
+    <message>
+        <location filename="../player.cpp" line="93"/>
+        <source>Invalid device &quot;%1&quot;!</source>
+        <translation>Problem z urządzeniem &quot;%1&quot;!</translation>
+    </message>
+    <message>
+        <location filename="../player.cpp" line="155"/>
+        <source>Player</source>
+        <translation>Odtwarzanie</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsTab</name>
+    <message>
+        <location filename="../settingstab.cpp" line="45"/>
+        <source>Audio device: </source>
+        <translation>Urządzenie audio: </translation>
+    </message>
+    <message>
+        <location filename="../settingstab.cpp" line="51"/>
+        <source>Select directory</source>
+        <translation>Wybierz katalog</translation>
+    </message>
+    <message>
+        <location filename="../settingstab.cpp" line="55"/>
+        <source>Songs directory: </source>
+        <translation>Katalog z pieśniami: </translation>
+    </message>
+</context>
+<context>
+    <name>SmallPanel</name>
+    <message>
+        <location filename="../smallpanel.cpp" line="86"/>
+        <source>Autoplay
+</source>
+        <translation>Autoodtwarzanie
+</translation>
+    </message>
+</context>
+</TS>

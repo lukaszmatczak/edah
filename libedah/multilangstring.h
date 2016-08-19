9c713ff2e78b1090950d9c5d49852062419b49b3
Lukasz Matczak <lukasz1235@gmail.com>
2016-08-19 13:41:15 +0200

    Created class "MultilangString"

---
+++ libedah/multilangstring.h
@@ -0,0 +1,47 @@
+/*
+    Edah
+    Copyright (C) 2016  Lukasz Matczak
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef MULTILANGSTRING_H
+#define MULTILANGSTRING_H
+
+#include <QMap>
+
+class MultilangString
+{
+public:
+    explicit MultilangString();
+    MultilangString(const MultilangString &other);
+    MultilangString(const MultilangString &&other);
+
+    QString toString() const;
+
+    operator QString() const;
+    MultilangString &operator =(const MultilangString &other);
+    QString &operator [](const QString &l);
+
+private:
+    static void setLang(const QString &l);
+
+    static QString lang;
+    QMap<QString, QString> data;
+
+    friend class MainWindow;
+};
+
+#endif // MULTILANGSTRING_H
